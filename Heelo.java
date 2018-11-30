3c7e829c12b3ed6964a52cfa12d8507001443c1a
kumm,Rama <rama.komm@gmail.com>
2018-11-29 22:17:10 -0800

    added hello world program

---
+++ Heelo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Akshara
+ *
+ */
+public class Heelo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i =0;i <= 10;i++)
+		{
+			System.out.println("Helloo World");
+		}
+	}
+
+}
